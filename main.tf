cb331c3cea359f1296bea085af35a8d9b51d06d5
praveen-mastek <82110612+praveen-mastek@users.noreply.github.com>
2021-04-27 23:01:33 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
+
+provider "aws" {
+  access_key = "ACCESS_KEY_HERE"
+  secret_key = "SECRET_KEY_HERE"
+  region     = "us-east-1"
+}
 resource "aws_instance" "MyFirstInstance" {
    ami = "ami-096cb92bb3580c759"
   instance_type = "t2.micro"
-  tags {
-    Name = "EC2_Instance_Terraform"
-    }
-  }
+}
