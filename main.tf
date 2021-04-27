996775d8a348c8f85bf57df1c795072df79c1bca
praveen-mastek <82110612+praveen-mastek@users.noreply.github.com>
2021-04-27 23:02:05 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 provider "aws" {
   access_key = "ACCESS_KEY_HERE"
   secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
+  region     = "eu-west-2"
 }
 resource "aws_instance" "MyFirstInstance" {
    ami = "ami-096cb92bb3580c759"
