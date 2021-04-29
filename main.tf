e3216a7d436acd96a3aacaf82e331f68b219bfb9
Praveen Sabba <Praveen61529@mastek.com>
2021-04-29 23:32:14 +0100

    jenkins update8

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
 provider "aws" {
   profile = "default"
   region  = "eu-west-2"
- }
+}
 resource "aws_instance" "MyFirstInstance" {
   ami           = "ami-096cb92bb3580c759"
   instance_type = "t2.micro"
 }
-
 
