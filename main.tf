043c3723e79b6d0550e23ed2790ec80ca92fc6b7
Praveen Sabba <Praveen61529@mastek.com>
2021-04-29 23:56:48 +0100

    jenkins update9

--- main.tf
+++ main.tf
@@ -1,9 +1,23 @@
-provider "aws" {
-  profile = "default"
-  region  = "eu-west-2"
-}
-resource "aws_instance" "MyFirstInstance" {
-  ami           = "ami-096cb92bb3580c759"
-  instance_type = "t2.micro"
+terraform {
+  # This module is now only being tested with Terraform 0.14.x. However, to make upgrading easier, we are setting
+  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
+  # forwards compatible with 0.14.x code.
+  required_version = ">= 0.14.10"
 }
 
+# Configure the AWS Provider
+provider "aws" {
+  profile = "default"
+  region = "eu-west-2"
+}
+
+# Create an EC2 instance
+resource "aws_instance" "example" {
+  # AMI ID for Amazon Linux AMI 2018.03.0 (HVM)
+  ami           = "ami-096cb92bb3580c759"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "example"
+  }
+}
